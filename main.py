f19a3b10dbb658a337284b9b06d0bea77171cfb6
arty <akochurka@bd4.ukr.education>
2025-07-20 11:27:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+with open('file.txt', 'r', encoding = "UTF-8") as file:
+    for data in file:
 
+        print(data)
 
+a = input('Добавити свій вірш:')
+if a == 'так':
+    b = input('Пиши:')
+    with open('just_file.txt', 'a') as file:
+        file.write(b)
+else:
+   print('Ок')
+with open('just_file.txt', 'r', encoding = "UTF-8") as file:
+    for data in file:
 
-def ob_det__at_image():
-    pass
-
-def main():
-    ob_det__at_image()
-
-if __name__ == '__main__':
-    main()
+        print(data)
